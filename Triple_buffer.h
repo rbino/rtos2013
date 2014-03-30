ea1dd393610474f1be9d18080d6a9b4e55978b66
Alessandro Mariani <ale.mariani91@gmail.com>
2014-03-30 16:02:59 +0200

    update Triple_buffer
    Signed-off-by: Alessandro Mariani <ale.mariani91@gmail.com>

--- Triple_buffer.h
+++ Triple_buffer.h
@@ -8,32 +8,64 @@
 #ifndef TRIPLE_BUFFER_H
 #define	TRIPLE_BUFFER_H
 
+#include "miosix.h"
+
+using namespace miosix;
+
 template<typename T, unsigned int size>
 class Triple_buffer {
 private:
     T buf[3][size];
-    T* current = buf[0][0];
-    T* ready = buf[1][0];
-    T* blocked = buf[2][0];
+    T* current, ready, blocked;
 
+    /**
+     * Swap the value of two pointers. Interrupts are disabled
+     * but the function is not thread safe in case of a preemptive
+     * scheduler.
+     * 
+     * @param a first pointer to swap
+     * @param b second pointer to swap
+     */
     void swap(T*& a, T*& b) {
+        FastInterruptDisableLock dLock;
         a = a ^ b;
         b = a ^ b;
         a = a ^ b;
     }
 
 public:
-    Triple_buffer();
-    ~Triple_buffer();
 
-    void get_writable(T *&buffer) {
+    Triple_buffer() {
+        T* current = buf[0];
+        T* ready = buf[1];
+        T* blocked = buf[2];
+    };
+    
+    ~Triple_buffer(){};
+
+    /**
+     * Change the current buffer with the ready one
+     * to permit the double buffering.
+     * 
+     * @param buffer pointer to new writable buffer
+     * @return size of the buffer
+     */
+    unsigned int get_writable(T *&buffer) {
         swap(current, ready);
         buffer = current;
+
+        return size;
     }
 
-    void get_readable(T *&buffer) {
-        swap (blocked, ready);
+    /**
+     * @param buffer pointer to new readable buffer
+     * @return size of the buffer
+     */
+    unsigned int get_readable(T *&buffer) {
+        swap(blocked, ready);
         buffer = blocked;
+
+        return size;
     }
 };
 
